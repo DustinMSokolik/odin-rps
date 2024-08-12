7bf7137afc82356f4b1cc575ea77a6ca17bcfdee
DustinMSokolik <Sokolikdustin@icloud.com>
2024-08-11 20:21:46 -0500

    Complete game

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # odin-rps
 Rock Paper Scissors console game w javascript
+
+This is my take on the Odin Project's rock paper scissor console game. 
+
+this demonstrates ability to plan for a problem and solve it.
---
+++ app.js
@@ -0,0 +1,66 @@
+//set variables that store both the computer choice and player choice
+let computerChoice;
+let playerChoice;
+
+//puts the whole script in a function to allow replay.
+function play() {
+    //Create a function that randomly selects Rock, Paper, or Scissors as a computer output to go against human player using the randomNumber variable
+    function getComputerChoice () {
+        //take the random number and output rock paper or scissors based on the number
+        switch (Math.floor(Math.random() * 3)) {
+            case 0:
+                computerChoice = "scissors";
+                break;
+            case 1:
+                computerChoice = "rock";
+                break;
+            case 2:
+                computerChoice = "paper";
+        };
+    };
+    //Initiate the function and store the output in the computerChoice variable
+    getComputerChoice();
+
+
+    //create a function that prompts the user to input their value and stores in in the variable playerChoice
+    function getPlayerChoice() {
+        let text;
+        while(true) { //Creates a loop to check if the correct option was altered otherwise re does prompt
+            //puts options to check against into an array
+            const options = ['rock' , 'paper' , 'scissors']
+            text = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+            if (options.includes(text)) {
+                break;
+            } else {
+                alert("Incorrect input, try again.");
+            }
+        }
+        playerChoice = text;
+        
+    }
+
+    getPlayerChoice();
+
+
+    //creates a function to test each option and outputs the winner
+
+    function results() {
+        console.log("You picked" + " " + playerChoice);
+        console.log("The computer picked" + " " + computerChoice);
+
+        if (playerChoice === computerChoice) {
+            alert("It's a tie!");
+        }else if (computerChoice == "rock" && playerChoice == "paper" 
+            || computerChoice == "paper" && playerChoice == "scissors"
+            || computerChoice == "scissors" && playerChoice == "rock") {
+                alert("You win!");
+        }else {
+            alert("You lose!");
+        }
+    }
+
+    results();
+}
+
+play()
